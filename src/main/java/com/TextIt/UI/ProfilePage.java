ae37581bf19d565fb75511cb83219005d65e2971
DhruvHarani1 <dhruvharani5@gmail.com>
2025-08-22 00:55:44 +0530

    completed profile page with changes

--- src/main/java/com/TextIt/UI/ProfilePage.java
+++ src/main/java/com/TextIt/UI/ProfilePage.java
@@ -11,15 +11,15 @@ public class ProfilePage {
     static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
-        int userid = Integer.parseInt(args[0]);
+       int userid = Integer.parseInt(args[0]);
 
         CommonMethods.userProfile(userid);
 
         int borderLength = 50;
         System.out.println(color("OPTIONS", PURPLE));
         System.out.println("-".repeat(borderLength));
-        System.out.println("[1] View Posts  [2] Edit Profile  [3] My Circles");
-        System.out.println("[4] Settings    [5] Privacy Mode  [6] Exit      ");
+        System.out.println("[1] View Posts  [2] Edit Profile  [3] Settings");
+        System.out.println("[4] Exit          ");
         System.out.println("-".repeat(borderLength));
         while (true) {
             System.out.println("Enter your choices :");
@@ -33,43 +33,13 @@ public class ProfilePage {
                     CommonMethods.editProfile(userid);
                     break;
                 case "3":
-                    my_circles();
+                    CommonMethods.openInNewCMD("com.TextIt.UI.SettingsPage " + userid);
                     break;
                 case "4":
-                    settings();
-                    break;
-                case "5":
-                    privacy_mode();
-                    break;
-                case "6":
                     return;
-                case "mood":
-//                    update_mood();
-                    break;
                 default:
                     System.out.println(RED + "Invalid choice. Please try again." + RESET);
             }
         }
     }
-
-//    private static void update_mood() {
-//        System.out.println("Enter your mood..." + GREEN);
-//        String nmood = sc.nextLine();
-//        System.out.println(CYAN + "Mood updated to :" + nmood + RESET);
-//        CommonMethods.pressEnterToContinue();
-//    }
-
-    private static void privacy_mode() {
-        System.out.println(CYAN + "Viewing posts....." + RESET);
-        CommonMethods.pressEnterToContinue();
-    }
-
-    private static void my_circles() {
-    }
-
-
-
-    private static void settings() {
-    }
-
 }
